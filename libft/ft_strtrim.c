986064414e932b2653c8dc856a6b9823c85b6a40
polltter <miguelr.silva@outlook.pt>
2022-10-30 19:27:08 +0000

    corrected some functions

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,40 +6,34 @@
 /*   By: mvenanci <mvenanci@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 10:38:03 by mvenanci          #+#    #+#             */
-/*   Updated: 2022/10/15 16:13:52 by mvenanci         ###   ########.fr       */
+/*   Updated: 2022/10/30 19:22:56 by mvenanci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_instr(char const *str, char c);
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len;
-	size_t	start;
+	int		len;
+	int		start;
 	char	*trim;
 
 	start = 0;
 	len = ft_strlen(s1) - 1;
-	while (ft_instr(set, s1[start]))
+	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-	while (ft_instr(set, s1[len]))
+	while (len >= 0 && ft_strchr(set, s1[len]))
 		len--;
+	if (start >= len)
+	{
+		trim = ft_calloc(1, 1);
+		return (trim);
+	}
 	len = len - start + 1;
 	trim = (char *)malloc(sizeof(char) * (len + 1));
+	if (!trim)
+		return (NULL);
 	trim[len] = 0;
 	trim = ft_memcpy(trim, s1 + start, len);
 	return (trim);
-}
-
-int	ft_instr(char const *str, char c)
-{
-	int	i;
-
-	i = -1;
-	while (str[++i])
-		if (str[i] == c)
-			return (1);
-	return (0);
 }
