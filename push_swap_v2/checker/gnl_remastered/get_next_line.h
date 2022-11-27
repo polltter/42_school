556a2abdf87078573980d4b983a47f165c152283
polltter <miguelr.silva@outlook.pt>
2022-11-27 16:29:33 +0000

    added gnl do checker

---
+++ push_swap_v2/checker/gnl_remastered/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvenanci <mvenanci@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/02 08:20:15 by mvenanci@st       #+#    #+#             */
+/*   Updated: 2022/11/27 16:13:53 by mvenanci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*strjoin(char *line, char *buffer);
+char	*get_next_line(int fd);
+
+
+#endif
