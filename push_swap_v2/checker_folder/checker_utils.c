b9ea337545a44631b1b801deef8e2b2fc549994b
polltter <miguelr.silva@outlook.pt>
2022-11-28 11:57:44 +0000

    small update on checker

--- push_swap_v2/checker_folder/checker_utils.c
+++ push_swap_v2/checker_folder/checker_utils.c
@@ -6,7 +6,7 @@
 /*   By: mvenanci <mvenanci@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 17:40:52 by mvenanci          #+#    #+#             */
-/*   Updated: 2022/11/28 11:49:53 by mvenanci         ###   ########.fr       */
+/*   Updated: 2022/11/28 11:56:37 by mvenanci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,17 @@ int	move_code(char *move)
 void	cut_lines(t_list **stack_a, t_list **stack_b)
 {
 	char	*s;
-
+	// int i = 0;
 	s = get_next_line(0);
 	while (s)
 	{
+		// ft_printf("%d, %s\n", i++, s);
 		aply_moves(stack_a, stack_b, s);
 		free(s);
 		s = get_next_line(0);
 	}
 	free(s);
-	if (is_sorted(*stack_a))
+	if (is_sorted(*stack_a) && !lstsize(*stack_b))
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
