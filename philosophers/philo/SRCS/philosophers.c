8cad54ffbf9797085170b308151e4bdd91a487a5
polltter <miguelr.silva@outlook.pt>
2023-01-26 14:55:16 +0000

    all

--- philosophers/philo/SRCS/philosophers.c
+++ philosophers/philo/SRCS/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: mvenanci <mvenanci@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 11:40:55 by mvenanci@st       #+#    #+#             */
-/*   Updated: 2023/01/26 13:37:19 by mvenanci         ###   ########.fr       */
+/*   Updated: 2023/01/26 14:54:43 by mvenanci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,22 @@ t_table	*table(void)
 	return (&table);
 }
 
+
 void	*run_threads(void *elem)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)elem;
-	
-	pthread_mutex_lock(philo->rigth);
-	pthread_mutex_lock(&philo->left);
-	printf("%d %ld\n", table()->dead++, ((t_philo *)philo)->id);
-	pthread_mutex_unlock(&philo->left);
-	pthread_mutex_unlock(philo->rigth);
+	while (!table()->dead)
+	{
+		pthread_mutex_lock(philo->rigth);
+		printf("philosopher %d %s\n", ((t_philo *)philo)->index, table()->msg[FORK]);
+		pthread_mutex_lock(&philo->left);
+		printf("philosopher %d %s\n", ((t_philo *)philo)->index, table()->msg[EAT]);
+		my_usleep(table()->times[EAT]);
+		pthread_mutex_unlock(&philo->left);
+		pthread_mutex_unlock(philo->rigth);
+	}
 	return (philo);
 }
 
@@ -37,12 +42,6 @@ void	init(t_elems *elem, void *o)
 {
 	(void)o;
 	pthread_create(&(((t_philo *)(elem->content))->id), NULL, run_threads, elem->content);
-}
-
-void	print_fork(t_elems *elem, void *o)
-{
-	(void)o;
-	printf("left: %p, right: %p\n", &(((t_philo *)(elem->content))->left), ((t_philo *)(elem->content))->rigth);
 }
 
 void	join_for_each(t_elems *elem, void *o)
@@ -60,7 +59,6 @@ int	main(int ac, char **av)
 		else
 			init_table(ft_atoi(av[1]), ft_atoi(av[2]), ft_atoi(av[3]), ft_atoi(av[4]), ft_atoi(av[5]));
 		array(table()->philos)->for_each(give_forks, NULL);
-		array(table()->philos)->for_each(print_fork, NULL);
 		array(table()->philos)->for_each(init, NULL);
 		array(table()->philos)->for_each(join_for_each, NULL);
 	}
