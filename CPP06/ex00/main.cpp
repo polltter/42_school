a8d63d7e7266994176f608346615d8e98cc344ce
miguel <miguelr.silva@outlook.pt>
2023-07-15 11:21:38 +0100

    added main

---
+++ CPP06/ex00/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by miguel on 02-07-2023.
+//
+#include "ScalarConverter.hpp"
+#include <iostream>
+int main(int ac, char **av) {
+
+    ScalarConverter a;
+    a.convert("42.1");
+
+    return 0;
+}
